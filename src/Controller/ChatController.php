846ed6e9ba6afbcec36612830ecc919d0be08ac3
Александр Ильченко <ilchenko.as@students.dvfu.ru>
2022-06-12 12:11:44 +1000

    Json not working anymore

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Controller;
 
+use Model\Message;
 use Model\User;
 use Twig\Environment;
+use Twig\Extension\DebugExtension;
 
 class ChatController
 {
@@ -14,13 +16,19 @@ class ChatController
     public function __construct(Environment $twig)
     {
         $this->twig = $twig;
+        $twig->addExtension(new DebugExtension());
     }
 
     public function render(iterable $messages, User $user) : string
     {
+        $msgArr = [];
+        foreach ($messages as $message) {
+            $msgArr[] = $messages;
+        }
+        $msgArr[] = new Message("Pokemon is the best game ever!", "Leon", "N");
         // Добавить View страницы и рендерить через неё
         return $this->twig->render('/Chat/chat.html.twig', [
-            'messages' => $messages,
+            'messages' => $msgArr,
             'user' => $user
         ]);
     }
